4c486a0ecf7db4b72221d69ec92c295e0a714932
fernando.caicedo <juan.caicedo1@alumnos.uneatlantico.es>
2025-01-14 12:59:57 +0100

    Lógica de elección ordenador y comparación con elección usuario

--- Práctica_examen_Final/Adivinaelnumero.java
+++ Práctica_examen_Final/Adivinaelnumero.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
+
 class AdivinaelNumero {
     public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
         int NUMERO_MINIMO = 0, NUMERO_MAXIMO = 100;
-        int INTENTOS_MINIMOS = 0, INTENTOS_MAXIMOS = 10;
+        int eleccionJugador;
+        int eleccionOrdenador = (int) (Math.random() * (NUMERO_MAXIMO - NUMERO_MINIMO + 1)) + NUMERO_MINIMO;
 
         System.out.println("Adivina el numero, escoge un numero entre " + NUMERO_MINIMO + " y " + NUMERO_MAXIMO++);
+        eleccionJugador = entrada.nextInt();
+        entrada.close();
 
+        while (true) {
+            if (eleccionJugador == eleccionOrdenador) {
+                System.out.println("Ganaste, tu elección fue: " + eleccionJugador + " y el ordenador eligio: "
+                        + eleccionOrdenador);
+            } else {
+                System.out.println("Perdiste, el número del ordenador era " + eleccionOrdenador);
+            }
+        }
     }
 
 }
