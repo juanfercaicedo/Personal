c00ed54f096853e080a5f3587c8806f7fbb4462a
fernando.caicedo <juan.caicedo1@alumnos.uneatlantico.es>
2025-01-14 17:08:02 -0500

    declaración variables

--- Práctica_examen_Final/Aspiradora.java
+++ Práctica_examen_Final/Aspiradora.java
@@ -1,5 +1,11 @@
 public class Aspiradora {
     public static void main(String[] args) {
-
+        final int PISO_LIMPIO = 0;
+        final int PISO_SUCIO = 1;
+        final int PISO_MUY_SUCIO = 2;
+        final int PISO_MUY_MUY_SUCIO = 3;
+        final int PISO_EXTREMADAMENTE_SUCIO = 4;
+        final int EJE_X = 0;
+        final int EJE_Y = 1;
     }
 }
