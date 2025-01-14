3e877b154249e0190b53648781eeac2f0e63ec6a
fernando.caicedo <juan.caicedo1@alumnos.uneatlantico.es>
2025-01-14 17:12:47 -0500

    implemetnación de superficie con matriz

--- Práctica_examen_Final/Aspiradora.java
+++ Práctica_examen_Final/Aspiradora.java
@@ -1,11 +1,25 @@
 public class Aspiradora {
     public static void main(String[] args) {
-        final int PISO_LIMPIO = 0;
-        final int PISO_SUCIO = 1;
-        final int PISO_MUY_SUCIO = 2;
-        final int PISO_MUY_MUY_SUCIO = 3;
-        final int PISO_EXTREMADAMENTE_SUCIO = 4;
+        final int SUPERFICIE_LIMPIA = 0;
+        final int SUPERFICIE_SUCIA = 1;
+        final int SUPERFICIE_MUY_SUCIA = 2;
+        final int SUPERFICIE_MUY_MUY_SUCIA = 3;
+        final int SUPERFICIE_EXTREMADAMENTE_SUCIA = 4;
         final int EJE_X = 0;
         final int EJE_Y = 1;
+
+        int[][] superficie = {
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+        };
+
     }
 }
