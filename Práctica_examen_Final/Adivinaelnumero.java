d53b360fa4fd251cdf70a844c11ae86701734a88
fernando.caicedo <juan.caicedo1@alumnos.uneatlantico.es>
2025-01-14 10:53:36 +0100

    declaración variables y problema

--- Práctica_examen_Final/Adivinaelnumero.java
+++ Práctica_examen_Final/Adivinaelnumero.java
@@ -1,5 +1,10 @@
-class AdivinaelNumero{
-    public static void main(String[]args){
-        
+class AdivinaelNumero {
+    public static void main(String[] args) {
+        int NUMERO_MINIMO = 0, NUMERO_MAXIMO = 100;
+        int INTENTOS_MINIMOS = 0, INTENTOS_MAXIMOS = 10;
+
+        System.out.println("Adivina el numero, escoge un numero entre " + NUMERO_MINIMO + " y " + NUMERO_MAXIMO++);
+
     }
+
 }
