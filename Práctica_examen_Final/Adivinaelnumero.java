ee91f0d6fe17e19e52f5a8024ffc90d21426717e
fernando.caicedo <juan.caicedo1@alumnos.uneatlantico.es>
2025-01-14 13:03:47 -0500

    contador y limite de intentos

--- Práctica_examen_Final/Adivinaelnumero.java
+++ Práctica_examen_Final/Adivinaelnumero.java
@@ -3,20 +3,32 @@ import java.util.Scanner;
 class AdivinaelNumero {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        int NUMERO_MINIMO = 0, NUMERO_MAXIMO = 100;
+        final int NUMERO_MINIMO = 0;
+        int NUMERO_MAXIMO = 100;
         int eleccionJugador;
+        int turnos = 0;
+        final int turnosMaximos = 15;
+        boolean adivinado = false;
+
         int eleccionOrdenador = (int) (Math.random() * (NUMERO_MAXIMO - NUMERO_MINIMO + 1)) + NUMERO_MINIMO;
 
         System.out.println("Adivina el numero, escoge un numero entre " + NUMERO_MINIMO + " y " + NUMERO_MAXIMO++);
-        eleccionJugador = entrada.nextInt();
-        entrada.close();
 
-        while (true) {
+        while (turnos < turnosMaximos && !adivinado) {
+            System.out.println("Estás en el intento #" + (turnos++));
+            eleccionJugador = entrada.nextInt();
+
             if (eleccionJugador == eleccionOrdenador) {
                 System.out.println("Ganaste, tu elección fue: " + eleccionJugador + " y el ordenador eligio: "
                         + eleccionOrdenador);
             } else {
                 System.out.println("Perdiste, el número del ordenador era " + eleccionOrdenador);
+            }
+
+            if (eleccionJugador < eleccionOrdenador) {
+                System.out.println("Tu número es muy bajo");
+            } else {
+                System.out.println("Tu número es muy alto");
             }
         }
     }
