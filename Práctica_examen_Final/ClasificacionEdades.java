9e63d2b7b48be1bfa17d69a39bd77c7f14f3a763
fernando.caicedo <juan.caicedo1@alumnos.uneatlantico.es>
2025-01-15 12:56:49 -0500

    Problema edades, se definieron las variables

---
+++ Práctica_examen_Final/ClasificacionEdades.java
@@ -0,0 +1,10 @@
+class ClasificacionEdades {
+    final int EDAD_MINIMA = 0;
+    final int PRIMERA_INFANCIA = 5;
+    final int INFANCIA = 11;
+    final int ADOLESCENCIA = 18;
+    final int JUVENTUD = 26;
+    final int ADULTEZ = 59;
+    final int EDAD_MAXIMA = 100;
+
+}
