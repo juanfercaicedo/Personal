b26c874cc1211bd9b27204ad332b47f2a07faa40
fernando.caicedo <juan.caicedo1@alumnos.uneatlantico.es>
2025-01-15 16:57:12 -0500

    declaraciones variables

--- Práctica_examen_Final/Mercadona.java
+++ Práctica_examen_Final/Mercadona.java
@@ -3,6 +3,16 @@ import java.util.Scanner;
 class Mercadona {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
+        final int HORAS_ABIERTO = 12;
+        final int MINUTOS_POR_HORA = 40;
+        final int TIEMPO_TOTAL = HORAS_ABIERTO * MINUTOS_POR_HORA;
+        final double PROBABILIDAD_LLEGADA_CLIENTE = 0.6;
+        final int PRODUCTOS_MINIMOS = 5, PRODUCTOS_MAXIMOS = 10;
+        final String PERSONA = "_O_ ", PRODUCTO = "[]";
+        int unaFila = 0;
+        int minutosCajaVacia = 0;
+        int caja1 = 0, caja2 = 0, caja3 = 0, caja4;
+        int horas = 9, minutos = 0;
 
         entrada.close();
     }
