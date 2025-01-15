f997ab32aa53fd55f7c2dc9c31cb0eecba11c7bd
fernando.caicedo <juan.caicedo1@alumnos.uneatlantico.es>
2025-01-15 18:09:36 -0500

    Probabilidad de llegada de cliente, logica para caja llena y caja vacia

--- Práctica_examen_Final/Mercadona.java
+++ Práctica_examen_Final/Mercadona.java
@@ -11,8 +11,37 @@ class Mercadona {
         final String PERSONA = "_O_ ", PRODUCTO = "[]";
         int unaFila = 0;
         int minutosCajaVacia = 0;
-        int caja1 = 0, caja2 = 0, caja3 = 0, caja4;
+        int caja1 = 0, caja2 = 0, caja3 = 0, caja4 = 0;
         int horas = 9, minutos = 0;
+
+        for (int tiempoActual = 1; tiempoActual <= TIEMPO_TOTAL; tiempoActual++) {
+            minutos++;
+            minutos = (minutos >= 60) ? 0 : minutos;
+            horas += (minutos >= 60) ? 1 : 0;
+        }
+
+        int llegaClienteNuevo = Math.random() <= PROBABILIDAD_LLEGADA_CLIENTE ? 1 : 0;
+        unaFila = unaFila + llegaClienteNuevo;
+
+        int[] cajas = { caja1, caja2, caja3, caja4 };
+
+        for (int i = 0; i < cajas.length; i++) {
+            if (cajas[i] <= 0 && unaFila > 0) {
+                unaFila--;
+                cajas[i] = (int) (Math.random() * (PRODUCTOS_MAXIMOS - PRODUCTOS_MINIMOS) + PRODUCTOS_MINIMOS);
+            }
+        }
+
+        for (int i = 0; i < cajas.length; i++) {
+            if (cajas[i] > 0) {
+                cajas[i] -= 1;
+            }
+        }
+
+        caja1 = cajas[0];
+        caja2 = cajas[1];
+        caja3 = cajas[2];
+        caja4 = cajas[3];
 
         entrada.close();
     }
