63da3955b851cce2e98c0c71788b37815ed882f3
fernando.caicedo <juan.caicedo1@alumnos.uneatlantico.es>
2025-01-15 12:51:40 -0500

    Problema terminado

--- Práctica_examen_Final/Aspiradora.java
+++ Práctica_examen_Final/Aspiradora.java
@@ -28,39 +28,89 @@ public class Aspiradora {
             moverAspiradora(posicionAspiradora, superficie);
             limpiarMundo(superficie, posicionAspiradora);
             imprimirMundo(superficie, posicionAspiradora);
-            superfieEstaSucia = estaSucia(superficie);
+            superfieEstaSucia = superfieEstaSucia(superficie);
             pause(0.5);
         }
     }
 
-    static void moverAspiradora(int[] posicionAspiradora, int[][] superficie){
+    static void moverAspiradora(int[] posicionAspiradora, int[][] superficie) {
 
-        int posicionPrevia = new int[] {posicionAspiradora[0], posicionAspiradora[1]};
+        int[] posicionPrevia = new int[] { posicionAspiradora[0], posicionAspiradora[1] };
 
-        int direcciones = {
-            { -1, 1 }, { 0, 1 }, { 1, 1 },
-            { -1, 0 }, { 0, 0 }, { 1, 0 },
-            { -1, -1 }, { 0, -1 }, { 1, -1 }
+        int[][] direcciones = {
+                { -1, 1 }, { 0, 1 }, { 1, 1 },
+                { -1, 0 }, { 0, 0 }, { 1, 0 },
+                { -1, -1 }, { 0, -1 }, { 1, -1 }
         };
 
-        int movimiento = direcciones[(int)*(Math.random()*direcciones.lenght)];
+        int[] movimiento = direcciones[(int) (Math.random() * direcciones.length)];
         posicionAspiradora[0] = posicionAspiradora[0] + movimiento[0];
         posicionAspiradora[1] = posicionAspiradora[1] + movimiento[1];
 
-        if(posicionInvalida(posicionAspiradora, superficie)){
+        if (posicionInvalida(posicionAspiradora, superficie)) {
             posicionAspiradora[0] = posicionPrevia[0];
             posicionAspiradora[1] = posicionPrevia[1];
         }
     }
 
-    static boolean posicionInvalida(int[] posicionAspiradora, int[][] superficie) {
-        return posicionAspiradora[0] < 0 || posicionAspiradora[0] >= superficie.lenght || posicionAspiradora[1] < 0
-                || posicionAspiradora >= superficie.length;
+    static boolean posicionInvalida(int[] position, int[][] surface) {
+        return position[0] < 0 || position[0] >= surface.length ||
+                position[1] < 0 || position[1] >= surface[0].length;
     }
 
     static void limpiarMundo(int[][] superficie, int[] posicionAspiradora) {
         if (superficie[posicionAspiradora[0]][posicionAspiradora[1]] > 0) {
             superficie[posicionAspiradora[0]][posicionAspiradora[1]]--;
+        }
+    }
+
+    static boolean superfieEstaSucia(int[][] superficie) {
+        return true;
+    }
+
+    static void imprimirMundo(int[][] unMapa, int[] posicionAspiradora) {
+        limpiarPantalla();
+        System.out.println("---".repeat(unMapa[0].length));
+        for (int row = 0; row < unMapa.length; row++) {
+            for (int column = 0; column < unMapa[row].length; column++) {
+                if (posicionAspiradora[0] == row && posicionAspiradora[1] == column) {
+                    System.out.println(mapaAspiradora());
+                } else {
+                    System.out.println(mapValueToTitle(unMapa[row][column]));
+                }
+            }
+            System.out.println();
+        }
+        System.out.println("---".repeat(unMapa[0].length));
+    }
+
+    static String mapaAspiradora() {
+        return ("0");
+    }
+
+    static String mapValueToTitle(int value) {
+        String[] tiles = { " . ", "...", "ooo", "OOO", "***" };
+        return tiles[value];
+    }
+
+    static void ensuciar(int[][] unMapa) {
+        for (int row = 0; row < unMapa.length; row++) {
+            for (int column = 0; column < unMapa[row].length; column++) {
+                unMapa[row][column] = (int) (Math.random() * 5);
+            }
+        }
+    }
+
+    static void limpiarPantalla() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
+    static void pause(double seconds) {
+        try {
+            Thread.sleep((int) (1000 * seconds));
+        } catch (InterruptedException e) {
+            //
         }
     }
 }
