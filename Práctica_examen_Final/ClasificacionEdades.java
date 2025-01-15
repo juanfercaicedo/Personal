a0d62282da05618cb90ca5fc0caacb22469363c4
fernando.caicedo <juan.caicedo1@alumnos.uneatlantico.es>
2025-01-15 12:57:57 -0500

    Implementacion de Scanner

--- Práctica_examen_Final/ClasificacionEdades.java
+++ Práctica_examen_Final/ClasificacionEdades.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class ClasificacionEdades {
     final int EDAD_MINIMA = 0;
     final int PRIMERA_INFANCIA = 5;
@@ -6,5 +8,7 @@ class ClasificacionEdades {
     final int JUVENTUD = 26;
     final int ADULTEZ = 59;
     final int EDAD_MAXIMA = 100;
+
+    Scanner entrada = new Scanner(System.in);
 
 }
