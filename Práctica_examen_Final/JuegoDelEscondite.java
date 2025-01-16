3a7307d25d132f15207e5533458faf11ada54f1f
fernando.caicedo <juan.caicedo1@alumnos.uneatlantico.es>
2025-01-16 16:54:10 +0100

    Condiciones nuevas de la pregunta 4 y 5

--- Práctica_examen_Final/JuegoDelEscondite.java
+++ Práctica_examen_Final/JuegoDelEscondite.java
@@ -10,6 +10,8 @@ class JuegoDelEscondite {
         final int Caseta = 5;
         final int Tobogan = 6;
         final double PROBABILIDAD_USUARIO_NO_VEA_NIÑOS = 0.1;
+        final double PROBABILIDAD_RUIDO = 0.05;
+        final double PROBABILIDAD_DE_CAMBIO = 0.3;
         int posicionNiño1, posicionNiño2, posicionNiño3, posicionNiño4;
         final int intentosMaximos = 12;
         int intentos = 0;
@@ -30,6 +32,36 @@ class JuegoDelEscondite {
         while (intentos < intentosMaximos && (!niño1Encontrado || !niño2Encontrado || !niño3Encontrado)) {
             System.out.println("\nIntento #" + (intentos++) + ": ¿Dónde quieres buscar? (1-6)");
             posicionNiño4 = entrada.nextInt();
+
+            if (intentos % 2 == 0 && Math.random() < PROBABILIDAD_RUIDO) {
+                int niñoDelatado = (Math.random() < 0.33)
+                        ? posicionNiño1
+                        : (Math.random() < 0.5)
+                                ? posicionNiño2
+                                : posicionNiño3;
+
+                System.out.println("¡Un niño hizo ruido y delató su posición!");
+                System.out.println("Pista: Un niño está en el lugar " + niñoDelatado);
+            } else {
+                System.out.println("Todo está en silencio...");
+            }
+
+            if (intentos == 6 && Math.random() < PROBABILIDAD_DE_CAMBIO) {
+                System.out.println("¡Los niños han cambiado de ubicación sigilosamente!");
+                int nuevaPosicion1 = (int) (Math.random() * 6) + 1;
+                int nuevaPosicion2 = (int) (Math.random() * 6) + 1;
+                while (nuevaPosicion2 == nuevaPosicion1) {
+                    nuevaPosicion2 = (int) (Math.random() * 6) + 1;
+                }
+                int nuevaPosicion3 = (int) (Math.random() * 6) + 1;
+                while (nuevaPosicion3 == nuevaPosicion1 || nuevaPosicion3 == nuevaPosicion2) {
+                    nuevaPosicion3 = (int) (Math.random() * 6) + 1;
+                }
+
+                posicionNiño1 = nuevaPosicion1;
+                posicionNiño2 = nuevaPosicion2;
+                posicionNiño3 = nuevaPosicion3;
+            }
 
             if (!niño1Encontrado && posicionNiño4 == posicionNiño1) {
                 if (Math.random() < PROBABILIDAD_USUARIO_NO_VEA_NIÑOS) {
@@ -56,7 +88,14 @@ class JuegoDelEscondite {
                 System.out.println("No hay niños en este lugar.");
             }
 
-            intentos++;
+            do {
+                posicionNiño2 = (int) (Math.random() * 6) + 1;
+            } while (posicionNiño2 == posicionNiño1);
+
+            do {
+                posicionNiño3 = (int) (Math.random() * 6) + 1;
+            } while (posicionNiño3 == posicionNiño1 || posicionNiño3 == posicionNiño2);
+
         }
 
         if (niño1Encontrado && niño2Encontrado && niño3Encontrado) {
