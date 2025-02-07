ed7f58341b821aa4135a18bba005f7cd3697eae5
fernando.caicedo <juan.caicedo1@alumnos.uneatlantico.es>
2025-02-07 18:40:19 -0500

    problema nuevo y codigo arreglado

--- Práctica_examen_Final/Aspiradora.java
+++ Práctica_examen_Final/Aspiradora.java
@@ -1,12 +1,5 @@
 public class Aspiradora {
     public static void main(String[] args) {
-        final int SUPERFICIE_LIMPIA = 0;
-        final int SUPERFICIE_SUCIA = 1;
-        final int SUPERFICIE_MUY_SUCIA = 2;
-        final int SUPERFICIE_MUY_MUY_SUCIA = 3;
-        final int SUPERFICIE_EXTREMADAMENTE_SUCIA = 4;
-        final int EJE_X = 0;
-        final int EJE_Y = 1;
 
         int[][] superficie = {
                 { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
