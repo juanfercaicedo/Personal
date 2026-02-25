c548ec82e9a1fb643cb25fc307008c2b5cd0252e
juanfercaicedo <juan.caicedo1@alumnos.uneatlantico.es>
2026-02-25 14:20:19 +0100

    Topologóa recurisvidad, código

---
+++ SegundoAño/EDA2/NotasClase25Feb/Topologia.java
@@ -0,0 +1,25 @@
+package SegundoAño.EDA2.NotasClase25Feb;
+
+class Topologia{
+    static void f(int n, String camino){
+        if(n==0){
+            System.out.println("Llegué a " + camino);
+            return;
+        } 
+        camino = camino + n + " ";
+        System.out.println("Elijo:  "+ n + "--->" + camino);
+        f(n-1, " ");
+        System.out.println("Saco: " + n + "--->" + camino);
+
+    }
+    
+    public static void main(String[] args) {
+        f(5, " " );
+    }
+    /*
+    Espacios topologios
+    1. Eliges
+    2. LLegas
+    3. Deshaces
+    */
+}
