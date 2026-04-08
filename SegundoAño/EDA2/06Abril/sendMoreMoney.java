fc0715bfdfff88f90cd9d49c9d3ef6e5cf181d2f
juanfercaicedo <juan.caicedo1@alumnos.uneatlantico.es>
2026-04-08 13:33:23 +0200

    REFACTOR

--- SegundoAño/EDA2/06Abril/sendMoreMoney.java
+++ SegundoAño/EDA2/06Abril/sendMoreMoney.java
@@ -1,9 +1,13 @@
-
-public class Criptoaritmo {
+public class sendMoreMoney {
 
     private static final char[] LETRAS = {'S', 'E', 'N', 'D', 'M', 'O', 'R', 'Y'};
-    private final static int[] valores = new int[256];
-    private final static boolean[] digitoAsignado = new boolean[10];
+
+    private static final char[] SEND = {'S', 'E', 'N', 'D'};
+    private static final char[] MORE = {'M', 'O', 'R', 'E'};
+    private static final char[] MONEY = {'M', 'O', 'N', 'E', 'Y'};
+
+    private static final int[] valores = new int[256];
+    private static final boolean[] digitoAsignado = new boolean[10];
 
     public static void main(String[] args) {
         for (int i = 0; i < 256; i++) {
@@ -12,26 +16,23 @@ public class Criptoaritmo {
 
         if (resolver(0)) {
             imprimirSolucion();
-        } else {
-            System.out.println("No se encontró ninguna solución.");
         }
     }
 
     private static boolean resolver(int indice) {
-        if (indice == LETRAS.length) {
+        if (indice == LETRAS.length) { 
             return esSumaValida();
         }
 
         char letraActual = LETRAS[indice];
 
         for (int digito = 0; digito <= 9; digito++) {
-
             if (!digitoAsignado[digito]) {
 
                 valores[letraActual] = digito;
                 digitoAsignado[digito] = true;
 
-                if (resolver(indice + 1)) {
+                if (resolver(indice + 1)) { 
                     return true;
                 }
 
@@ -43,27 +44,36 @@ public class Criptoaritmo {
         return false;
     }
 
+    private static int asignarNumero(char[] letras) {
+        int numero = 0;
+        for (int i = 0; i < letras.length; i++) {
+            numero = numero * 10 + valores[letras[i]];
+        }
+        return numero;
+    }
+
     private static boolean esSumaValida() {
         if (valores['S'] == 0 || valores['M'] == 0) {
             return false;
         }
 
-        int send = valores['S'] * 1000 + valores['E'] * 100 + valores['N'] * 10 + valores['D'];
-        int more = valores['M'] * 1000 + valores['O'] * 100 + valores['R'] * 10 + valores['E'];
-        int money = valores['M'] * 10000 + valores['O'] * 1000 + valores['N'] * 100 + valores['E'] * 10 + valores['Y'];
+        int send = asignarNumero(SEND);
+        int more = asignarNumero(MORE);
+        int money = asignarNumero(MONEY);
 
         return (send + more == money);
     }
 
     private static void imprimirSolucion() {
-        int send = valores['S'] * 1000 + valores['E'] * 100 + valores['N'] * 10 + valores['D'];
-        int more = valores['M'] * 1000 + valores['O'] * 100 + valores['R'] * 10 + valores['E'];
-        int money = valores['M'] * 10000 + valores['O'] * 1000 + valores['N'] * 100 + valores['E'] * 10 + valores['Y'];
+        int send = asignarNumero(SEND);
+        int more = asignarNumero(MORE);
+        int money = asignarNumero(MONEY);
 
         System.out.println("  " + send);
         System.out.println("+ " + more);
         System.out.println("-------");
         System.out.println(" " + money);
+
         System.out.println("\nAsignación de letras:");
         for (char letra : LETRAS) {
             System.out.println(letra + " = " + valores[letra]);
